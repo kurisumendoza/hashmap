f4b7bd5879e3eb059e4d2ada1ae9e03b66e7688d
Kris Mendoza <thekittengallery@gmail.com>
2025-02-21 21:11:54 +0800

    Implement hash method

--- hashmap.js
+++ hashmap.js
@@ -1,6 +1,17 @@
 class HashMap {
   constructor() {
-    capacity = 16;
-    loadFactor = 0.75;
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
   }
 }
